1f5c8cf5d991e5995234f3342bfc1a723888d842
Hannah Wohl-Machado <hannahmachado@yahoo.ca>
2023-07-28 13:02:38 -0400

    added list component

--- src/pages/form/Form.jsx
+++ src/pages/form/Form.jsx
@@ -1,5 +1,19 @@
+import styles from './form.module.css';
+import List from '../../components/list/List';
+
 const FormPage = () => {
-  return <div>form</div>;
+  return (
+    <div className={styles.container}>
+      <div className={`${styles.left} ${styles.innerDiv}`}>
+        <h1 className={styles.heading}>Stay updated!</h1>
+        <h2 className={styles.heading2}>
+          Join 60,000+ product managers receiving monthly updates on:
+        </h2>
+        <List />
+      </div>
+      <div className={`${styles.right} ${styles.innerDiv}`}></div>
+    </div>
+  );
 };
 
 export default FormPage;
