2a7cfd7fdbdb5b69e93255a212fb09e0b18e8d54
Hannah Wohl-Machado <hannahmachado@yahoo.ca>
2023-07-31 11:41:03 -0400

    created form component

---
+++ src/components/form/Form.jsx
@@ -0,0 +1,23 @@
+import styles from './form.module.css';
+import Button from '../Button';
+
+const Form = () => {
+  return (
+    <form className={styles.form}>
+      <div className={styles.inputContainer}>
+        <label className={styles.label} htmlFor='email'>
+          Email address
+        </label>
+        <input
+          className={styles.input}
+          type='text'
+          placeholder='email@company.com'
+          id='email'
+        />
+      </div>
+      <Button text='Subscribe to monthly newsletter' className='newsletter' />
+    </form>
+  );
+};
+
+export default Form;
