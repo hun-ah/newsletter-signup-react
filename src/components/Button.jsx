49166fe6250758c9972274a9ff48264bc0ab49c1
Hannah Wohl-Machado <hannahmachado@yahoo.ca>
2023-08-01 10:20:47 -0400

    added ternary operator if link prop is given

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,14 @@
 import styles from './button.module.css';
+import { Link } from 'react-router-dom';
 
-const Button = ({ text }) => {
-  return <button className={styles.button}>{text}</button>;
+const Button = ({ text, link }) => {
+  return link ? (
+    <Link className={styles.button} to='/'>
+      {text}
+    </Link>
+  ) : (
+    <button className={styles.button}>{text}</button>
+  );
 };
 
 export default Button;
