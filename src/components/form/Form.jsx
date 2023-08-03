263654c4c2eb9aeb52fbacfc394e31d036efe54a
Hannah Wohl-Machado <hannahmachado@yahoo.ca>
2023-08-03 10:17:13 -0400

    added page navigtion on successful form submit and email validation

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -1,18 +1,44 @@
 import styles from './form.module.css';
 import Button from '../Button';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Form = () => {
+  const [err, setErr] = useState(false);
+  const [userInput, setUserInput] = useState({ email: '' });
+  const navigate = useNavigate();
+
+  const handleForm = (e) => {
+    e.preventDefault();
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    if (emailRegex.test(userInput)) {
+      setErr(false);
+      navigate('/success');
+    } else {
+      setErr(true);
+    }
+  };
+
+  const handleInput = (e) => {
+    setUserInput(e.target.value);
+  };
+
   return (
-    <form className={styles.form}>
+    <form className={styles.form} onSubmit={() => handleForm(event)}>
       <div className={styles.inputContainer}>
-        <label className={styles.label} htmlFor='email'>
-          Email address
-        </label>
+        <div className={styles.inputLabels}>
+          <label className={styles.label} htmlFor='email'>
+            Email address
+          </label>
+          {err && <span className={styles.error}>Valid email required</span>}
+        </div>
         <input
-          className={styles.input}
+          className={`${styles.input} ${err ? styles.inputErr : styles.input}`}
           type='text'
           placeholder='email@company.com'
           id='email'
+          onChange={() => handleInput(event)}
+          onFocus={() => setErr(false)}
         />
       </div>
       <Button text='Subscribe to monthly newsletter' />
